7102633fe24c05e77ac34ed7ed8aae39994553ba
Gautam Jha <gjha133@gmail.com>
2023-04-02 20:16:26 +0530

    Fix navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import Avatar from '../Avatar/Avatar'
 import logo from '../../assets/logo.png'
 import search from '../../assets/search-solid.svg'
 import './Navbar.css'
+import bars from "../../assets/bars-solid.svg";
 
 
 const Navbar = () => {
@@ -13,23 +14,28 @@ const Navbar = () => {
   return (
     <nav className="main-nav">
       <div className="navbar">
+        <button className="slide-in-icon">
+          <img src={bars} alt="bars" width="15" />
+        </button>
         <div className="navbar-1">
           <Link to="/" className="nav-item nav-logo">
             <img src={logo} alt="logo" />
           </Link>
           <Link to="/" className="nav-item nav-btn res-nav">
-            About
-          </Link>
-          <Link to="/" className="nav-item nav-btn res-nav">
-            Products
-          </Link>
-          <Link to="/" className="nav-item nav-btn res-nav">
-            For Teams
-          </Link>
+              About
+            </Link>
+            <Link to="/" className="nav-item nav-btn res-nav">
+              Products
+            </Link>
+            <Link to="/" className="nav-item nav-btn res-nav">
+              For Teams
+            </Link>
           <form>
             <input type="text" placeholder="Search..." />
             <img src={search} alt="search" width="18" className="search-icon" />
           </form>
+        </div>
+        <div className="navbar-2">
           {
             User === null ? (
               <Link to={'/Auth'} className='nav-item nav-links'>
@@ -46,8 +52,8 @@ const Navbar = () => {
                 >
                   <Link
                     to={'/User'}
-                    style={{ 
-                      color: "white", textDecoration: "none" 
+                    style={{
+                      color: "white", textDecoration: "none"
                     }}
                   >
                     G
