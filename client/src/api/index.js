6b17f4342a958c7f9b1ae60d90b8a0f3ff49eb7d
Gautam Jha <gjha133@gmail.com>
2023-04-05 09:20:40 +0530

    Deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: "http://localhost:5000"})
+const API = axios.create({ baseURL: "https://stack-overflow-clone-server-2bw6.onrender.com"})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
