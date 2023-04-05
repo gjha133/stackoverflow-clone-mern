3b0bf6379abf9c36c47c4a1016d5b3bd7bba2080
Gautam Jha <gjha133@gmail.com>
2023-04-05 07:56:05 +0530

    Design change in navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -56,14 +56,14 @@ const Navbar = () => {
               Products
             </Link>
             <Link to="/" className="nav-item nav-btn res-nav">
-              For Teams
+              Teams
             </Link>
-          <form>
-            <input type="text" placeholder="Search..." />
-            <img src={search} alt="search" width="18" className="search-icon" />
-          </form>
         </div>
         <div className="navbar-2">
+          <form>
+            <input type="text" placeholder="   Search..." />
+            <img src={search} alt="search" width="18" className="search-icon" />
+          </form>
           {
             User === null ? (
               <Link to={'/Auth'} className='nav-item nav-links'>
