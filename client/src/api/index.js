d2838f617eb2b77fc15d4bd089309995f2b08981
Gautam Jha <53974342+gjha133@users.noreply.github.com>
2023-04-15 07:39:25 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: "http://localhost:5000"})
+const API = axios.create({ baseURL: "https://stack-overflow-clone-server-vq27.onrender.com"})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
